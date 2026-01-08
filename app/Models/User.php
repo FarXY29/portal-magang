1813f7bb81d2f7b4d9f4dee5c2b55ca4bbaca12f
hambali1129 <hambali1129@gmail.com>
2026-01-08 19:00:13 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,7 +28,7 @@ class User extends Authenticatable
         'nik',
         'phone',
         'asal_instansi', 
-        'major', // Kolom Jurusan Mahasiswa
+        'major', // Kolom Jurusan Mahasiswa / Siswa
     ];
 
     /**
